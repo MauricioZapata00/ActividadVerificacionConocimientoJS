9093820b3bd8c520b80780cc45f2132e46d48b12
MauricioZapata00 <maurozac123@gmail.com>
2022-02-03 21:16:41 -0500

    Queda el juego listo para su prueba, se detectan las colisiones por medio de un código que ya se tenía previamente validado su funcionamiento y también donde se calculaba el ángulo de salida de la misma pelota. Solo se detectan colisiones de los objetos cuadrados con la pelota, con las paredes no se logra detectar y animar la colisión. Queda a gusto de los demás desarrolladores validar este caso.

--- main.js
+++ main.js
@@ -30,9 +30,12 @@
         this.board = board;
         this.speed_y = 0;
         this.speed_x = 3;
+        this.speed = 3;
         board.ball = this;
         this.kind = "circle";
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI/12;
 
         
     }
@@ -40,6 +43,27 @@
         move: function(){
             this.x += this.speed_x * this.direction;
             this.y += this.speed_y;
+        },
+        get width(){
+            return this.radius * 2;
+        },
+        get height(){
+            return this.radius * 2;
+        },
+        collision: function(bar){//Se genera la reacción de la pelota, se recibe la barra
+            //de la colisión
+            var relative_intersect_y = ( bar.y + (bar.height/2)) - this.y;
+            var normalize_intersect_y = relative_intersect_y/(bar.height/2);
+            //Se calcula el ángulo con el que se mueve la pelota
+            this.bounce_angle = normalize_intersect_y * this.max_bounce_angle;
+            this.speed_y = this.speed * (-1) * Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+            if(this.x > (this.board.width/2)){//Se cambia la dirección si se logra colisionar
+                //con una barra
+                this.direction = -1;
+            }else{
+                this.direction = 1;
+            }
         }
     }
 })();
@@ -88,10 +112,19 @@
                 draw(this.ctx,el);
             };
         },
+        check_collisions: function(){
+            for(var i = this.board.bars.length - 1 ; i>=0; i--){
+                var bar = this.board.bars[i];
+                if(hit(bar, this.board.ball)){
+                    this.board.ball.collision(bar);
+                }
+            };
+        },
         play: function(){
             if(this.board.playing){//Se mueve la bola solo si está en juego
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
             /*
@@ -101,6 +134,30 @@
             */
 
         }
+    }
+
+    function hit(a,b){//Revisa si objeto a colisiona con b
+        var hit = false;
+        //Colisiones horizontales
+        if(b.x + b.width >= a.x && b.x < a.x + a.width){
+            //Colisiones verticales
+            if(b.y + b.height >= a.y && b.y < a.y + a.height){
+                hit = true;
+            }
+        }
+        //Colision de a con b
+        if(b.x <= a.x && b.x + b.width >= a.x + a.width){
+            if(b.y <= a.y && b.y + b.height >= a.y + a.height){
+                hit = true;
+            }
+        }
+        //Colision de b con a
+        if(a.x <= b.x && a.x + a.width >= b.x + b.width){
+            if(a.y <= b.y && a.y + a.height >= b.y + b.height){
+                hit = true;
+            }
+        }
+        return hit;
     }
 
     function draw(ctx, element){
@@ -130,8 +187,8 @@
 
 //Se colocan aquí las variables para que todas las clases tengan acceso a ellas
 var board = new Board(800,400);
-var bar = new Bar(20,100,40,100,board);
-var bar_2 = new Bar(735,100,40,100,board);
+var bar_2 = new Bar(20,100,40,100,board);
+var bar = new Bar(735,100,40,100,board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
 var ball = new Ball(350,100,10,board);
